aed6ddcf31030cbd585463764ca73607bc0dbbcb
Varun Deshpande <varundeshpande.vd@gmail.com>
2023-11-13 10:58:29 -0500

    Added snippets

--- array.sh
+++ array.sh
@@ -1,5 +1,5 @@
 array=() # declare an array
-arrVar1=("PHP" "MySQL" "Bash" "Oracle") # delcare and initialize an array
+array=("PHP" "MySQL" "Bash" "Oracle") # delcare and initialize an array
 array+=("foo")
 array+=("bar")
 array[${#array[@]}]="baz" # add new element at the end of the array
@@ -9,4 +9,40 @@ do
      echo $value
 done
 
+# length of array
 echo "items in array: ${#array[@]}"
+
+
+
+###
+## Declare an array
+array=();
+echo $array;
+
+## Add elements to an array
+array+=("one")
+
+array+=("two")
+
+echo ${array[@]}
+```
+one two
+```
+
+array+=("three")
+echo ${array[@]}
+```one two three```
+
+## remove elements from an array
+delete=two
+echo ${array[@]/$delete}
+```one three```
+
+echo ${array[@]}
+```one two three```
+
+array=( "${array[@]/$delete}" )
+echo ${array[@]}
+```one three```
+
+
