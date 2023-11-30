925c7a573179e0900103441be51945f209b9255d
Varun Deshpande <varun.deshpande@smilecdr.com>
2023-11-29 23:27:22 -0500

    Added array functions

--- array.sh
+++ array.sh
@@ -45,4 +45,28 @@ array=( "${array[@]/$delete}" )
 echo ${array[@]}
 ```one three```
 
+# Use unset to delete an element in an array
+for i in ${!bgpids[@]}
+        do
 
+            p=${bgpids[i]}
+            # Check if the folder /proc/pid exists. 
+            # If exists then the pid still running, if no then pid is finished execution
+            # Check the exit code of the finished pid and remove the pid from the bgpids array
+            if [[ ! -d "/proc/${p}" ]]
+            then
+                if wait $p
+                then
+                    log "Blob backup subprocess ${p} succeeded";
+                else
+                    err "Blob backup subprocess ${p} failed";
+                    return_code=1;
+                    break 2;
+                    # return $return_code;
+                fi
+
+                # remove the pid from the bgpids array
+                unset bgpids[i]
+            fi
+
+        done
